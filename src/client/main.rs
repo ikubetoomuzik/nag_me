b5199bf886086fa61282ea2674e2e482222fb320
Curtis Jones <ikubetoomuzik@gmail.com>
2021-03-21 15:48:49 -0400

    started working with async child processes

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,6 +1,16 @@
 // binary testing
 //
 
+use async_std::{process, task};
+
 fn main() {
+    task::block_on(async {
+        println!("Child process output...");
+        process::Command::new("/home/curtis/scripts/test.sh")
+            .stdout(process::Stdio::inherit())
+            .status()
+            .await
+            .expect("failed.");
+    });
     println!("In progress...");
 }
